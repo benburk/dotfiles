e389000856944d8f91c6a212e1ff6f4eb6c114a3
Ben Burk <benburk@outlook.com>
2019-10-13 12:17:11 -0400

    add no-ignore-vcs to fzf commands

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,19 +1,19 @@
-# Start tmux on login
-if test "$TERM" = "xterm-kitty"
-    if status is-interactive
-        and not set -q TMUX
-        exec tmux new -A -s base
-    end
-end
+# # Start tmux on login
+# if test "$TERM" = "xterm-kitty"
+#     if status is-interactive
+#         and not set -q TMUX
+#         exec tmux new -A -s base
+#     end
+# end
 
 # Remove welcome message
 set fish_greeting
 
 
 # FZF Settings
-set -x FZF_DEFAULT_COMMAND 'fd --follow ""'
+set -x FZF_DEFAULT_COMMAND 'fd --follow --no-ignore-vcs""'
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-set -x FZF_ALT_C_COMMAND 'fd --follow --type directory ""'
+set -x FZF_ALT_C_COMMAND 'fd --follow --no-ignore-vcs --type directory ""'
 
 
 set -Ux EDITOR kak
