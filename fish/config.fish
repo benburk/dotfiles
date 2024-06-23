80ed2bf8548b677f304f85e031f9b1fbec9f746a
benburk <21958895+benburk@users.noreply.github.com>
2024-06-23 00:04:28 -0500

    add shebang, set variables in interactive mode

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -gx EDITOR hx
+#!/usr/bin/env fish
 
 set -gx FZF_DEFAULT_COMMAND "fd --type f"
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
@@ -8,11 +8,11 @@ set -gx FZF_ALT_C_OPTS "--preview 'tree -C {}'"
 
 
 if status is-interactive
-    # Suppress fish welcome message
-    set fish_greeting
-
+    set fish_greeting # Suppress fish welcome message
     fish_config theme choose "Nord"
     fish_config prompt choose "Arrow"
+    
+    set -gx EDITOR hx
 
     # https://github.com/kovidgoyal/kitty/issues/268
     alias clear="printf '\033[2J\033[3J\033[1;1H'"
