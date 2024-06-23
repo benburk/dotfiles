d7d049e59be7984d08e5c6f878e8486d67c870fb
benburk <21958895+benburk@users.noreply.github.com>
2024-06-23 00:04:27 -0500

    only print repo name if things are unpushed

--- scripts/git-status.sh
+++ scripts/git-status.sh
@@ -1,6 +1,8 @@
 for file in $(fd $1 -d 1); do
-    echo -e "\033[0;31m${file}\033[0m"
     cd $file
+    if [[ $(git status --porcelain) ]]; then
+        echo -e "\033[0;31m${file}\033[0m"
+    fi
     git status --porcelain
     cd ..
 done
