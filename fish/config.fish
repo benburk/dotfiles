c706cbc93c37f6eae9635af37755d81d5550bd8f
benburk <21958895+benburk@users.noreply.github.com>
2024-06-23 00:04:29 -0500

    side-by-side for delta, secret keys for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,11 @@
 #!/usr/bin/env fish
 
-set -gx FZF_DEFAULT_COMMAND "fd --type f"
-set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-set -gx FZF_CTRL_T_OPTS "--preview 'bat -n --color=always {}'"
-set -gx FZF_ALT_C_COMMAND "fd --type d"
-set -gx FZF_ALT_C_OPTS "--preview 'tree -C {}'"
-
 fish_add_path --move /usr/local/opt/fzf/bin
 fish_add_path --move $HOME/.cargo/bin
+
+set -gx OPENAI_API_KEY (
+    security find-generic-password -a "$USER" -s "OPENAI_API_KEY" -w
+)
 
 if status is-interactive
     set fish_greeting # Suppress fish welcome message
@@ -15,6 +13,12 @@ if status is-interactive
     fish_config prompt choose "Arrow"
     
     set -gx EDITOR hx
+
+    set -gx FZF_DEFAULT_COMMAND "fd --type f"
+    set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+    set -gx FZF_CTRL_T_OPTS "--preview 'bat -n --color=always {}'"
+    set -gx FZF_ALT_C_COMMAND "fd --type d"
+    set -gx FZF_ALT_C_OPTS "--preview 'tree -C {}'"
 
     # https://github.com/kovidgoyal/kitty/issues/268
     alias clear="printf '\033[2J\033[3J\033[1;1H'"
